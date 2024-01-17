0c1a4d1c4aaf4a0fb33a5c1a4f7672993c315d6d
imaneouarzi <ima55ouar@gmail.com>
2024-01-17 14:37:28 +0100

    updates in AuthUserController

--- app/Http/Controllers/Api/User/AuthUserController.php
+++ app/Http/Controllers/Api/User/AuthUserController.php
@@ -14,41 +14,8 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthUserController extends Controller
 {
-    // public function register(Request $request){
-    //     $rules = [
-    //         'name'=>'required',
-    //         'email'=>'required|string|email|unique:users,email',
-    //         'password'=>'required'
-    //     ];
-
-    //     $validator = Validator::make($request->all(),[
-    //         "name"=>"required",
-    //         "email"=>"required|string|email|unique:users,email",
-    //         "password"=>"required"
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //        return response()->json($validator->errors());
-    //     }
-
-    //     User::create([
-    //         'name'=> $request->name,
-    //         'email'=>$request->email,
-    //         'password'=>Hash::make($request->password),
-    //     ]);
-
-    //     $user = User::first();
-    //     $token = JWTAuth::fromUser($user);
-    //     return response()->json($token);
-    // }
-
 
     public function register(Request $request){
-        $rules = [
-            'name'=>'required',
-            'email'=>'required|string|email|unique:users,email',
-            'password'=>'required'
-        ];
 
         $validator = Validator::make($request->all(),[
             "name"=>"required",
@@ -75,45 +42,6 @@ class AuthUserController extends Controller
         $token = JWTAuth::fromUser($user,$payload);
         return response()->json(compact('token'));
     }
-
-
-
-
-
-
-    // public function login( Request $request){
-    //     $validator = Validator::make($request -> all(),[
-    //      'email' => 'required|string|email|max:255',
-    //      'password'=> 'required'
-    //     ]);
-
-    //     if ($validator -> fails()) {
-    //         # code...
-    //         return response()->json($validator->errors());
-
-    //     }
-    //     $credentials = $request->only('email','password');
-    //     // add some data to payload token when login
-    //     $payload = JWTFactory::sub($credentials)
-    //     ->role('user')
-    //     ->country('us')
-    //     ->make();
-    //     $token = JWTAuth::attempt( $credentials,$payload);
-    //     try{
-    //         if (! $token ) {
-    //             # code...
-    //             return response()->json( ['error'=> 'invalid username and password'],401);
-    //         }
-    //     }catch(JWTException $e){
-
-    //       return response()->json( ['error'=> 'could not create token'],500);
-    //     }
-
-
-    //     return response()->json( compact('token'));
-
-    // }
-
 
     public function login( Request $request){
         $validator = Validator::make($request -> all(),[
@@ -178,4 +106,68 @@ class AuthUserController extends Controller
         return response()->json( compact('token'));
 
     }
+
+ // public function login( Request $request){
+    //     $validator = Validator::make($request -> all(),[
+    //      'email' => 'required|string|email|max:255',
+    //      'password'=> 'required'
+    //     ]);
+
+    //     if ($validator -> fails()) {
+    //         # code...
+    //         return response()->json($validator->errors());
+
+    //     }
+    //     $credentials = $request->only('email','password');
+    //     // add some data to payload token when login
+    //     $payload = JWTFactory::sub($credentials)
+    //     ->role('user')
+    //     ->country('us')
+    //     ->make();
+    //     $token = JWTAuth::attempt( $credentials,$payload);
+    //     try{
+    //         if (! $token ) {
+    //             # code...
+    //             return response()->json( ['error'=> 'invalid username and password'],401);
+    //         }
+    //     }catch(JWTException $e){
+
+    //       return response()->json( ['error'=> 'could not create token'],500);
+    //     }
+
+
+    //     return response()->json( compact('token'));
+
+    // }
+
+
+    /** ******************************************** */
+
+        // public function register(Request $request){
+    //     $rules = [
+    //         'name'=>'required',
+    //         'email'=>'required|string|email|unique:users,email',
+    //         'password'=>'required'
+    //     ];
+
+    //     $validator = Validator::make($request->all(),[
+    //         "name"=>"required",
+    //         "email"=>"required|string|email|unique:users,email",
+    //         "password"=>"required"
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //        return response()->json($validator->errors());
+    //     }
+
+    //     User::create([
+    //         'name'=> $request->name,
+    //         'email'=>$request->email,
+    //         'password'=>Hash::make($request->password),
+    //     ]);
+
+    //     $user = User::first();
+    //     $token = JWTAuth::fromUser($user);
+    //     return response()->json($token);
+    // }
 }
